584575c0d6a76943ee14d94dd3a73592ad0bd7fa
Evan Zook <130406055+etldrz@users.noreply.github.com>
2023-08-16 15:54:15 -0400

    Update equality_point_test.R

---
+++ code/testing/equality_point_test.R
@@ -0,0 +1,34 @@
+diver <- readRDS('diver.rds')
+
+#diver covers alpha: 0.1, lambda: 0, omega: 0, p:.1
+  #for rows 1:1e4 theta: 0.08. for rows 1e4+1:2e4 theta: 0.8
+
+foo <- function(choice){
+  lim <- c(1, 1e4)
+  miny <- 4
+  maxy <- 7
+  if(choice=="upper"){
+    miny <- 20
+    maxy <- 30
+    lim <- c(1e4 + 1, 2e4)
+  }
+  
+  x <- seq(1, 52, by=.1)
+  plot(NA, xlim=c(mean(diver[lim[1]:lim[2],5] + 1) - 3, mean(diver[lim[1]:lim[2],5] + 1) + 3), 
+       ylim=c(miny, maxy))
+  
+  for(i in lim[1]:lim[2]){
+    c <- diver[i,]
+    
+    lines(x=x, y=(x*c[1] + c[2]), col='black', lwd=1)
+    lines(x=x, y=(x*c[3] + c[4]), col='black', lwd=1)
+  }
+  
+  abline(v=c(min(diver[lim[1]:lim[2],5] + 1), max(diver[lim[1]:lim[2],5] + 1)), col='red')
+  abline(v=total.tp[1,7], col='black')
+  
+  #legend('topleft' legend=pa)
+}
+
+foo('lower')
+
